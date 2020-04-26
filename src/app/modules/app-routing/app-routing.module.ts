76d16123b309471f55c0fc017c7a31369f0b7df8
Joel Caples <Joel.Caples@orion.com>
2020-04-26 12:11:34 -0500

    stuff

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PortletComponent }   from '../../components/portlet/portlet.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/portlet', pathMatch: 'full' },
+  { path: 'portlet', component: PortletComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
