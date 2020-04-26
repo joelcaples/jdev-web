ccfa2bdcb7e8cd178f89a5937e38d2ae026fc100
Joel Caples <Joel.Caples@orion.com>
2020-04-25 22:42:23 -0500

    styles

--- src/app/components/portlet/portlet.component.ts
+++ src/app/components/portlet/portlet.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortletComponent implements OnInit {
 
+  public mode:string="BLANK";
+  public asOfDate:Date=new Date();
+
   constructor() { }
 
   ngOnInit(): void {
