f66b050069f3555aa563869338f506b31d1931ed
Joel Caples <jmcaples@gmail.com>
2020-05-16 00:20:59 -0500

    tachyons

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app/modules/app-routing/app-routing.module';
 import { AppComponent } from './app/components/app/app.component';
-import { RequestQueueComponent } from './app/components/request-queue/request-queue.component';
-import { WebScraperComponent } from './app/components/web-scraper/web-scraper.component';
+import { RequestQueueComponent } from './app/widgets/request-queue/request-queue.component';
+import { WebScraperComponent } from './app/widgets/web-scraper/web-scraper.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
