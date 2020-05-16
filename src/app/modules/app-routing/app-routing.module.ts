3499122b8f5982e2c0179a8c29c59cb16735c762
Joel Caples <jmcaples@gmail.com>
2020-05-16 09:40:05 -0500

    Added Router

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RequestQueueComponent } from 'src/app/widgets/request-queue/request-queue.component';
+import { WebScraperComponent } from 'src/app/widgets/web-scraper/web-scraper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+  // { path: 'heroes', component: HeroesComponent }
+  { path: 'requestqueue', component: RequestQueueComponent },
+  { path: 'webscraper', component: WebScraperComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
