4b1fcb510be85a04d874c4f5627692753c6cca71
Joel Caples <jmcaples@gmail.com>
2020-05-23 08:15:09 -0500

    Moved app stuff back to default location

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app/modules/app-routing/app-routing.module';
-import { AppComponent } from './app/components/app/app.component';
-import { RequestQueueComponent } from './app/widgets/request-queue/request-queue.component';
-import { WebScraperComponent } from './app/widgets/web-scraper/web-scraper.component';
+import { AppRoutingModule } from './modules/app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { RequestQueueComponent } from './widgets/request-queue/request-queue.component';
+import { WebScraperComponent } from './widgets/web-scraper/web-scraper.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TechComponent } from './app/components/tech/tech.component';
-import { ResourcesComponent } from './app/components/resources/resources.component';
-import { WidgetDashboardComponent } from './app/widgets/widget-dashboard/widget-dashboard.component';
-import { ComicsComponent } from './app/components/comics/comics.component';
+import { TechComponent } from './components/tech/tech.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { WidgetDashboardComponent } from './widgets/widget-dashboard/widget-dashboard.component';
+import { ComicsComponent } from './components/comics/comics.component';
 import { FormsModule } from '@angular/forms';
 import { AgGridModule } from 'ag-grid-angular';
 
